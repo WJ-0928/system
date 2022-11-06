f8c182174ca3a1626ed189b9fdcf92cfc87d11c1
WJ-0928 <49092641+WJ-0928@users.noreply.github.com>
2022-11-06 12:46:31 +0800

    Add files via upload

---
+++ Sort.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+//≤Â»Î≈≈–Ú
+void InsertSort(int array[], int size);
+//œ£∂˚≈≈–Ú
+void ShellSort(int* array, int size);
+//∂—≈≈–Ú
+void HeapSort(int* array, int size);
+
+
+
+
+void TestSort();
